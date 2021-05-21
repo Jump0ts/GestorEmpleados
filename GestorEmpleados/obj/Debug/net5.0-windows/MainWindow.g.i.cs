cad71ba09c48b4baaaa5b7ab853c48120903eb1e
Jump0ts <josanfersal@gmail.com>
2021-05-21 19:00:07 +0200

    1.0 corregido
    Añadido texto al botón de buscar mientras no funcione la imagen y eliminación de las imágenes que al final no he usado.

--- GestorEmpleados/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ GestorEmpleados/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace GestorEmpleados {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GestorEmpleados;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GestorEmpleados;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
